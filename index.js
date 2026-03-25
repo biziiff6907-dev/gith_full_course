88df8c9052d36a9f4269e0ddd11bf55f063f0da0
藤田 敢 <is-fujita@jtsnet.co.jp>
2026-03-25 13:40:06 +0900

    関数を呼び出した

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 function greet() {
   console.log("Hello from the main branch!");
 }
-=======
-function greet() {
-  console.log("Hello from the local branch!");
-}
->>>>>>> 883aa1d (私達の修正)
+
+greet();
