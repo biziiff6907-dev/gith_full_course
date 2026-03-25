bc52c25beaf30d22adf20530e123ac51bb2dfcbb
藤田 敢 <is-fujita@jtsnet.co.jp>
2026-03-25 13:35:58 +0900

    修正

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 function greet() {
   console.log("Hello from the main branch!");
 }
+=======
+function greet() {
+  console.log("Hello from the local branch!");
+}
+>>>>>>> 883aa1d (私達の修正)
